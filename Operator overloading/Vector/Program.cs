65d9325558551524ea2c67c7fd655015ac09403d
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2022-05-12 23:21:34 +0200

    Changing Polish names to English

--- Operator overloading/Vector/Program.cs
+++ Operator overloading/Vector/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 
-struct Wektor
+struct Vektor
 {
     int x;
     int y;
     int z;
-    public Wektor(int x, int y, int z)
+    public Vektor(int x, int y, int z)
     {
         this.x = x;
         this.y = y;
@@ -16,29 +16,29 @@ struct Wektor
     {
         return "X: " + x + " Y: " + y + " Z: " + z;
     }
-    public static Wektor operator +(Wektor w)
+    public static Vektor operator +(Vektor w)
     {
-        return new Wektor(w.x, w.y, w.z);
+        return new Vektor(w.x, w.y, w.z);
     }
-    public static Wektor operator -(Wektor w)
+    public static Vektor operator -(Vektor w)
     {
-        return new Wektor(-w.x, -w.y, -w.z);
+        return new Vektor(-w.x, -w.y, -w.z);
     }
-    public static Wektor operator *(Wektor w, int n)
+    public static Vektor operator *(Vektor w, int n)
     {
-        return new Wektor(w.x * n, w.y * n, w.z * n);
+        return new Vektor(w.x * n, w.y * n, w.z * n);
     }
-    public static Wektor operator /(Wektor w, int n)
+    public static Vektor operator /(Vektor w, int n)
     {
-        return new Wektor(w.x / n, w.y / n, w.z / n);
+        return new Vektor(w.x / n, w.y / n, w.z / n);
     }
     public override bool Equals(object obj)
     {
         if (obj == null)
             return false;
-        else if (obj is Wektor)
+        else if (obj is Vektor)
         {
-            if (x == ((Wektor)obj).x && y == ((Wektor)obj).y && z == ((Wektor)obj).z)
+            if (x == ((Vektor)obj).x && y == ((Vektor)obj).y && z == ((Vektor)obj).z)
                 return true;
             else
                 return false;
@@ -50,21 +50,21 @@ struct Wektor
     {
         return HashCode.Combine(x, y, z);
     }
-    public static bool operator ==(Wektor w, Wektor v)
+    public static bool operator ==(Vektor w, Vektor v)
     {
         if (w.x == v.x && w.y == v.y && w.z == v.z)
             return true;
         else
             return false;
     }
-    public static bool operator !=(Wektor w, Wektor v)
+    public static bool operator !=(Vektor w, Vektor v)
     {
         if (w == v)
             return false;
         else
             return true;
     }
-    public static double operator ~(Wektor w)
+    public static double operator ~(Vektor w)
     {
         return (double)Math.Sqrt(Math.Pow(w.x, 2) + Math.Pow(w.y, 2) + Math.Pow(w.z, 2));
     }
@@ -73,10 +73,10 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Wektor w1 = new Wektor(1, 2, 3);
-        Wektor w2 = new Wektor(1, 2, 5);
-        Wektor w3 = new Wektor(1, 2, 3);
-        Wektor w4 = new Wektor(2, 4, 6);
+        Vektor w1 = new Vektor(1, 2, 3);
+        Vektor w2 = new Vektor(1, 2, 5);
+        Vektor w3 = new Vektor(1, 2, 3);
+        Vektor w4 = new Vektor(2, 4, 6);
         Console.WriteLine(w3 * 2);
         Console.WriteLine();
         Console.WriteLine(w1 == w3);
